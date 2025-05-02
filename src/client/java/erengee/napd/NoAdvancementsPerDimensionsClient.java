7acd4990b94533448f0a7ba9c53a137e788aafc1
kutaxoff <kutaxoff@icloud.com>
2025-05-02 12:00:54 +0300

    Initial commit

---
+++ src/client/java/erengee/napd/NoAdvancementsPerDimensionsClient.java
@@ -0,0 +1,10 @@
+package erengee.napd;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NoAdvancementsPerDimensionsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
